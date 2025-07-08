4a174bf308ce77a4080d59480c1a1f67e8fd1b19
naturano-tech <andriamamonjisoafaniry@gmail.com>
2025-07-08 17:42:37 +0200

    main

--- Lora_v1_esp32U4_recep_envoie_gs/Lora_v1_esp32U4_recep_envoie_gs.ino
+++ Lora_v1_esp32U4_recep_envoie_gs/Lora_v1_esp32U4_recep_envoie_gs.ino
@@ -1,48 +1,71 @@
 #include "Arduino.h"
 #include "LoRa_E32.h"
 #include <WiFi.h>
-kaiza
-// Remplace par le nom de ton réseau Wi-Fi et ton mot de passe
+#include <HTTPClient.h>
+
+//LoRa_E32 e32ttl(&Serial2, 15, 4, 2); // RX, AUX, M0, M1
+LoRa_E32 e32ttl(&Serial2, 15, 19, 18); // RX, AUX, M0, M1
 const char* ssid = "PIX-LINK-2.4G";
 const char* password = "naturanowifi1";
-// Configuration des broches pour ESP32 38 pins
-LoRa_E32 e32ttl(&Serial2, 15, 4, 2); // RX, AUX, M0, M1
+
+String googleScriptUrl = "https://script.google.com/macros/s/AKfycbxr9yEF-C5c6YXGV-L7W4TLHRVDN3qeNP8DljwKXyFsx8KxU1vjIk8MAM4ALbw8UzP2WQ/exec";
 
 void setup() {
   Serial.begin(9600);
   delay(500);
 
-  Serial.println();
-  Serial.print("Connexion au Wi-Fi : ");
-  Serial.println(ssid);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
+  e32ttl.begin();
+
+  Serial.println("Module LoRa initialisé - Connexion Wi-Fi...");
+
   WiFi.begin(ssid, password);
-  // Attente de la connexion
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  Serial.println();
-  Serial.println("Connecté au Wi-Fi !");
-  Serial.print("Adresse IP : ");
+  Serial.println("\nWi-Fi connecté !");
+  Serial.print("IP: ");
   Serial.println(WiFi.localIP());
-
-  Serial2.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
-  e32ttl.begin();
-  Serial.println("Module LoRa initialisé - Prêt à envoyer des messages");
-
 }
 
 void loop() {
-Serial.println(receiving_data());
-}
-String receiving_data(){
   if (e32ttl.available() > 1) {
     ResponseContainer rc = e32ttl.receiveMessage();
     if (rc.status.code != 1) {
-      return rc.status.getResponseDescription();
+      Serial.println("Erreur LoRa: " + rc.status.getResponseDescription());
     } else {
-      return rc.data;
+      String message = rc.data;
+      message.trim();
+      Serial.print("Message reçu: ");
+      Serial.println(message);
+
+      if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+      
+        String url = googleScriptUrl + "?data=" + message; 
+        Serial.println("URL envoyée: " + url);
+
+        http.begin(url);
+        int httpCode = http.GET();
+        
+        if (httpCode == HTTP_CODE_OK) {
+          String payload = http.getString();
+          // Serial.println("Réponse Google: " + payload);
+        } else {
+          Serial.printf("Erreur HTTP: %d\n", httpCode);
+        }
+        http.end();
+      } else {
+        Serial.println("Wi-Fi déconnecté !");
+        WiFi.reconnect(); // Tentative de reconnexion
+      }
     }
   }
+
+  if (Serial.available()) {
+    String input = Serial.readString();
+    e32ttl.sendMessage(input);
+  }
+  delay(1000); // Évite les requêtes trop rapides
 }
